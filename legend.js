8a4d1057dfd6b9ea6e24de5e5209ce61cef3da7d
Richard Carder <rcarder@wellspringadvisors.com>
2017-05-26 16:01:28 -0400

    both legends

--- legend.js
+++ legend.js
@@ -1,249 +1,146 @@
-var legend1=d3.select('#legend').append('svg').append('g')  ;
+var svgWidth = 420;
 
-    legend1
+var startValue=svgWidth/2-10;
+
+var spacing= 5;
+
+
+var legend=d3.select('#circuitry')
+  .append('center')
+  .append('svg')
+  .attr("text-anchor", "middle")
+  .style('height',20);
+
+legend
+      .append('text')
+      .attr('x',60)
+      .attr('y',14)
+      .attr('class','legendtext')
+      .text('1 Mile').style('fill','#CECECE');
+
+
+    legend
       .append('circle')
-      .attr('class','point')
-      .attr('cx',20)
-      .attr('cy',20)
+      .attr('fill','#eff3ff')
+      .attr('cx',92)
+      .attr('cy',10)
       .attr('r',8);
 
-    legend1
-      .append('text')
-      .attr('x',30)
-      .attr('y',25)
-      .attr('class','legendtext')
-      .text('Tier 1').style('fill','#CECECE');
+    legend
+      .append('circle')
+      .attr('fill','#c6dbef')
+      .attr('cx',112)
+      .attr('cy',10)
+      .attr('r',8);
 
-    var legend2=d3.select('#legend').select('svg').append('g')
+    legend
+      .append('circle')
+      .attr('fill','#9ecae1')
+      .attr('cx',132)
+      .attr('cy',10)
+      .attr('r',8);
+        legend
+      .append('circle')
+      .attr('fill','#6baed6')
+      .attr('cx',152)
+      .attr('cy',10)
+      .attr('r',8);
+        legend
+      .append('circle')
+      .attr('fill','#4292c6')
+      .attr('cx',172)
+      .attr('cy',10)
+      .attr('r',8);
+        legend
+      .append('circle')
+      .attr('fill','#2171b5')
+      .attr('cx',192)
+      .attr('cy',10)
+      .attr('r',8);
+        legend
+      .append('circle')
+      .attr('fill','#084594')
+      .attr('cx',212)
+      .attr('cy',10)
+      .attr('r',8);
+    legend
+      .append('text')
+      .attr('x',247)
+      .attr('y',14)
+      .attr('class','legendtext')
+      .text('2.8 Miles').style('fill','#CECECE');
+
+
+
+
+var legend2=d3.select('#Population')
+  .append('center')
+  .append('svg')
+  .attr("text-anchor", "middle")
+  .style('height',60)
+  .style('width',svgWidth);
+
+legend2
+      .append('text')
+      .attr('x',function (d) { return startValue-(rscale(25000)+2*rscale(20000)+2*rscale(15000)+6*rscale(10000)+4*spacing); })
+      .attr('y',34)
+      .attr('class','legendtext')
+      .text('10,000').style('fill','#CECECE');
+
 
     legend2
       .append('circle')
-      .attr('class','point2')      
-      .attr('cx',20)
-       .attr('cy',40)
-       .attr('r',8);
+      .attr('fill','none')
+      .attr('stroke','#CECECE')
+      .attr('cx',function (d) { return startValue-(rscale(25000)+2*rscale(20000)+2*rscale(15000)+rscale(10000)+3*spacing); })
+      .attr('cy',30)
+      .attr('r', function (d) { return rscale(10000); });
+
+    legend2
+      .append('circle')
+      .attr('fill','none')
+      .attr('stroke','#CECECE')
+      .attr('cx',function (d) { return startValue-(rscale(25000)+2*rscale(20000)+rscale(15000)+2*spacing); })
+      .attr('cy',30)
+      .attr('r', function (d) { return rscale(15000); });
+
+    legend2
+      .append('circle')
+      .attr('fill','none')
+      .attr('stroke','#CECECE')
+      .attr('cx',function (d) { return startValue-(rscale(25000)+rscale(20000)+spacing);})
+      .attr('cy',30)
+      .attr('r', function (d) { return rscale(20000); });
+
+    legend2
+      .append('circle')
+      .attr('fill','none')
+      .attr('stroke','#CECECE')
+      .attr('cx',startValue)
+      .attr('cy',30)
+      .attr('r', function (d) { return rscale(25000); });
+
+    legend2
+      .append('circle')
+      .attr('fill','none')
+      .attr('stroke','#CECECE')
+      .attr('cx',function (d) { return startValue+(rscale(25000)+rscale(30000)+spacing);})
+      .attr('cy',30)
+      .attr('r', function (d) { return rscale(30000); });
+
+    legend2
+      .append('circle')
+      .attr('fill','none')
+      .attr('stroke','#CECECE')
+      .attr('cx',function (d) { return startValue+(rscale(25000)+2*rscale(30000)+rscale(35000)+2*spacing);})
+      .attr('cy',30)
+      .attr('r', function (d) { return rscale(35000); });
 
     legend2
       .append('text')
-      .attr('x',30)
-      .attr('y',45)
+      .attr('x',function (d) { return startValue+(rscale(25000)+2*rscale(30000)+3*rscale(35000)+3*spacing);})
+      .attr('y',34)
       .attr('class','legendtext')
-      .text('Tier 2')
-       .style('fill','#CECECE');
-
-  var legend3=d3.select('#legend').select('svg').append('g')
-
-    legend3
-      .append('circle')
-      .attr('class','point3')      
-      .attr('cx',20)
-       .attr('cy',60)
-       .attr('r',8);
-
-    legend3
-      .append('text')
-      .attr('x',30)
-      .attr('y',65)
-      .attr('class','legendtext')
-      .text('Tier 3')
-       .style('fill','#CECECE');
-  
-    var legend4=d3.select('#legend').select('svg').append('g')      .attr('class','legendtext') 
-
-    legend4
-      .append('circle')
-      .attr('class','point4') 
-      .attr('cx',20)
-       .attr('cy',80)
-       .attr('r',8);
-
-    legend4
-      .append('text')
-      .attr('x',30)
-      .attr('y',85)
-      .attr('class','legendtext')
-      .text('Tier 4')
-       .style('fill','#CECECE');
-
-
-var legend5=d3.select('#legend').select('svg').append('g')
-
-    legend5
-      .append('circle')
-      .attr('class','point5')      
-      .attr('cx',20)
-       .attr('cy',100)
-       .attr('r',8);
-
-    legend5
-      .append('text')
-      .attr('x',30)
-      .attr('y',105)
-      .attr('class','legendtext')
-      .text('Tier 5')
-       .style('fill','#CECECE');
-
-var legend6=d3.select('#legend').select('svg').append('g')
-
-    legend6
-      .append('circle')
-      .attr('class','point6')      
-      .attr('cx',20)
-       .attr('cy',120)
-       .attr('r',8);
-
-    legend6
-      .append('text')
-      .attr('x',30)
-      .attr('y',125)
-      .attr('class','legendtext')
-      .text('Tier 6')
-       .style('fill','#CECECE');
-
-var legend7=d3.select('#legend').select('svg').append('g')
-
-    legend7
-      .append('circle')
-      .attr('class','point7')      
-      .attr('cx',20)
-       .attr('cy',140)
-       .attr('r',8);
-
-    legend7
-      .append('text')
-      .attr('x',30)
-      .attr('y',145)
-      .attr('class','legendtext')
-      .text('Tier 7')
-       .style('fill','#CECECE');
-
-var legend8=d3.select('#legend').select('svg')
-  .attr('height',310)
-  .attr('width',100)
-  .append('g')
-
-    legend8
-      .append('circle')
-      .attr('class','point8')      
-      .attr('cx',20)
-       .attr('cy',160)
-       .attr('r',8);
-
-    legend8
-      .append('text')
-      .attr('x',30)
-      .attr('y',165)
-      .attr('class','legendtext')
-      .text('Tier 8')
-       .style('fill','#CECECE');
-
-var legend9=d3.select('#legend').select('svg').append('g')
-
-    legend9
-      .append('circle')
-      .attr('class','point9')      
-      .attr('cx',20)
-       .attr('cy',180)
-       .attr('r',8);
-
-    legend9
-      .append('text')
-      .attr('x',30)
-      .attr('y',185)
-      .attr('class','legendtext')
-      .text('Tier 9')
-       .style('fill','#CECECE');
-
-var legend10=d3.select('#legend').select('svg').append('g')
-
-    legend10
-      .append('circle')
-      .attr('class','point10')      
-      .attr('cx',20)
-       .attr('cy',200)
-       .attr('r',8);
-
-    legend10
-      .append('text')
-      .attr('x',30)
-      .attr('y',205)
-      .attr('class','legendtext')
-      .text('Tier 10')
-       .style('fill','#CECECE');
-
-  var legend11=d3.select('#legend').select('svg').append('g')
-
-    legend11
-      .append('circle')
-      .attr('class','sizelegend')      
-      .attr('cx',20)
-       .attr('cy',230)
-       .attr('r',13.2)
-       .style('fill','none')
-       .style('stroke','#CECECE');
-
-    legend11
-      .append('text')
-      .attr('x',35)
-      .attr('y',235)
-      .attr('class','legendtext')
-      .text('$50 Value')
-      .style('fill','#CECECE');
-
-   var legend12=d3.select('#legend').select('svg').append('g')
-
-    legend12
-      .append('circle')
-      .attr('class','sizelegend')      
-      .attr('cx',20)
-       .attr('cy',257)
-       .attr('r',10.45)
-       .style('fill','none')
-       .style('stroke','#CECECE');
-
-    legend12
-      .append('text')
-      .attr('x',35)
-      .attr('y',262)
-      .attr('class','legendtext')
-      .text('$30 Value')
-      .style('fill','#CECECE');
-
-     var legend13=d3.select('#legend').select('svg').append('g')
-
-    legend13
-      .append('circle')
-      .attr('class','sizelegend')      
-      .attr('cx',20)
-       .attr('cy',281)
-       .attr('r',7.725)
-       .style('fill','none')
-       .style('stroke','#CECECE');
-
-    legend13
-      .append('text')
-      .attr('x',35)
-      .attr('y',286)
-      .attr('class','legendtext')
-      .text('$15 Value')
-      .style('fill','#CECECE');
-
-       var legend14=d3.select('#legend').select('svg').append('g')
-
-    legend14
-      .append('circle')
-      .attr('class','sizelegend')      
-      .attr('cx',20)
-       .attr('cy',303)
-       .attr('r',2.736)
-       .style('fill','none')
-       .style('stroke','#CECECE');
-
-    legend14
-      .append('text')
-      .attr('x',35)
-      .attr('y',308)
-      .attr('class','legendtext')
-      .text('$1 Value')
-      .style('fill','#CECECE');
-
+      .html(function(d) { return '35,000';})
+      .style('fill','#CECECE')
+      .style('text-align','left');
